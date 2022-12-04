cd71f3d923a5a9fa070b4155820ccf51fffaa9b4
lhdgsn <liamdhodgson@gmail.com>
2022-12-03 20:45:09 -0500

    Added data generating distributions

--- VAE.py
+++ VAE.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class Encoder(nn.Module):
     """
-    p(z|x)
+    Implements the approximate posterior q(z|x)
     """
     def __init__(self, n_features, z_dim, layer_sizes):
         """
@@ -45,7 +45,7 @@ class Decoder(nn.Module):
     Conditional: p(x|z,c)
     Auto-regressive: p(x_i|z,x_{j<i})
     """
-    def __init__(self, z_dim, n_features, layer_sizes, is_autoregressive):
+    def __init__(self, z_dim, n_features, layer_sizes, generative_model, is_autoregressive):
         """
         n_features (int): number of input features per observation
         z_dim (int): number of latent variables
@@ -66,8 +66,23 @@ class Decoder(nn.Module):
                 self.model_layers.append(nn.Linear(layer_sizes[i_layer], layer_sizes[i_layer+1]))
                 self.model_layers.append(nn.ReLU())
         
-        # output layer
-        self.output_layer = nn.Linear(layer_sizes[-1], n_features)
+        # output layers (depends on generative model)
+        self.output_layers = nn.ModuleDict()
+        self.output_activations = nn.ModuleDict()
+        
+        if(generative_model == 'gaussian'):
+            self.output_layers['mean'] = nn.Linear(layer_sizes[-1], n_features)
+            self.output_layers['sigma'] = nn.Linear(layer_sizes[-1], n_features)
+        
+        elif(generative_model == 'nb'):
+            self.output_layers['n'] = nn.Linear(layer_sizes[-1], n_features)
+            self.output_activations['n'] = nn.ReLU()
+            self.output_layers['p'] = nn.Linear(layer_sizes[-1], n_features)
+            self.output_activations['p'] = nn.Sigmoid()
+        
+        elif(generative_model == 'bernoulli'):
+            self.output_layers['p'] = nn.Linear(layer_sizes[-1], n_features)
+            self.output_activations['p'] = nn.Sigmoid()
 
     def autoregressive_mask(self):
         pass
@@ -79,14 +94,31 @@ class Decoder(nn.Module):
         if(self.is_autoregressive):
             pass
 
-        return self.output_layer(x)
+        outs = []
+        for (param, output_layer) in self.output_layers.items():
+            x_out = output_layer(x)
+            if(param in self.output_activations.keys()):
+                x_out = self.output_activations[param](x_out)
+            outs.append(x_out)
+        
+        return outs
 
 class VAE(nn.Module):
-    def __init__(self, n_features, z_dim, layer_sizes, activation=None, n_batches=1, is_autoregressive=False, is_conditional=False, seed=0):
+    def __init__(self, n_features, z_dim, layer_sizes, activation=None, generative_model='gaussian', n_batches=1, is_autoregressive=False, is_conditional=False, seed=0):
         """
+        n_features (int): number of input features per observation
+        z_dim (int): number of latent variables
+        layer_size (list): number of hidden units for each encoder layer
+        activation:
+        generative_model (string): data generative model, parametrized by decoder neural network ('gaussian', 'bernoulli', 'nb', 'zinb')
+        n_batches (int): number of batches present in data (default 1)
+        is_autoregressive (bool): 
+        is_conditional (bool):
+        seed (int): seed for random number generator
         """
         self.input_dim = n_features
         self.z_dim = z_dim
+        self.generative_model = generative_model
         self.is_autoregressive = is_autoregressive
         self.is_conditional = is_conditional
 
@@ -96,7 +128,7 @@ class VAE(nn.Module):
         self.encoder = Encoder(n_features, z_dim, layer_sizes)
 
         # define decoder network
-        self.decoder = Decoder(n_features, z_dim, layer_sizes, is_autoregressive)
+        self.decoder = Decoder(n_features, z_dim, layer_sizes, generative_model, is_autoregressive)
 
         # latent batch offset vector
         if(n_batches>1):
@@ -104,14 +136,30 @@ class VAE(nn.Module):
         else:
             self.batch_offset = torch.zeros(z_dim, n_batches) # no batch correction
 
-    def elbo_loss(self, x, x_out):
+    def generate_data(self, params):
+        if(self.generative_model == 'bernoulli'):
+            return self.rng.binomial(n=1, p=params[0])
+        elif(self.generative_model == 'gaussian'):
+            return self.rng.normal(loc=params[0], scale=params[1])
+        elif(self.generative_model == 'nb'):
+            return self.rng.negative_binomial(n=params[0], p=params[1])
+
+    def elbo_loss(self, means, vars, x, x_out):
         """
         """
-        pass
+        # KL divergence between true and approximate prior
+        # equivalent to a regularization term
+        # see eq. 10 from Kingma and Welling, 2014
+        KL_divergence = 0.5 * torch.sum(1 + torch.log(vars**2) - means**2 - vars**2)
+
+        # negative log-likelihood of generated data
+        # equivalent to reconstruction loss
+        nll = torch.log(x_out)
+
+        # negative because we are minimizing loss
+        return -(KL_divergence + nll)
 
     def forward(self, x, condition_labels=None):
-        """
-        """
         # append condition labels if relevant (may be continuous)
         if(self.is_conditional):
             x = torch.concat((x, condition_labels), dim=1)
@@ -125,7 +173,14 @@ class VAE(nn.Module):
         if(self.is_conditional):
             z = torch.concat((z, condition_labels), dim=1)
 
-        x_out = self.decoder(z)
+        # generate observation based on sample from latent distribution
+        generative_model_params = self.decoder(z)
 
-        return x_out
+        # generate data
+        x_out = self.generate_data(generative_model_params)
+
+        # calculate loss
+        elbo = self.elbo_loss(means, vars, x, x_out)
+
+        return x_out, elbo
 
